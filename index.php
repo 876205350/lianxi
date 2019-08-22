21eba0579084d98d807ca23c3029ea31c3f437fa
liuhaiyang.com <876205350@qq.com>
2019-08-22 19:02:32 +0800

    修改index

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-echo 'hello word';
-echo 'git hello';
+class person{
+  public static $name;
+  public $age;
+  private $sex;
+  function __construct($name,$age,$sex){
+      self::$name = $name;
+      $this ->age = $age;
+      $this ->sex = $sex;
+  }
+  private static function setname(){
+    self::$name = "刘海洋";
+  }
+  public function my(){
+      $this->setname();
+      return self::$name;
+  }
+}
+
+$person = new person("张三",23,"男");
+echo "性别：".$person->my();
